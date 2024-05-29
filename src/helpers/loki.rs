6ac2828bccb96d93125a8ec3c4e6122f4332eafc
Eliot Eikenberry <eliote@liatrio.com>
2024-05-29 15:36:05 -0400

    feature(df): implement df loki query

--- src/helpers/loki.rs
+++ src/helpers/loki.rs
@@ -1,26 +1,96 @@
 use std::env;
-use serde::de::DeserializeOwned;
-use serde::Serialize;
+use anyhow::Result;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize, Deserializer};
 
 use crate::helpers::error::AppError;
 
-pub async fn query<D, R>(data: D) -> Result<R, AppError>
+#[derive(Serialize, Debug)]
+pub struct QueryParams {
+  pub query: String,
+  pub start: String,
+  pub end: String
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct QueryResultDataEntryStream {
+  pub action: Option<String>,
+  pub created_at: Option<DateTime<Utc>>,
+  pub environment_name: Option<String>,
+  pub merged_sha: Option<String>,
+  pub merged_at: Option<DateTime<Utc>>,
+  pub repository_name: Option<String>,
+  pub service_name: Option<String>,
+  pub team_name: Option<String>,
+  pub topics: Option<Vec<String>>,
+  pub deployment_environment: Option<String>,
+  pub deployment_state: Option<String>,
+}
+
+fn deserialize_inner<'de, D>(deserializer: D) -> Result<QueryResultDataEntryValue, D::Error>
 where
-  R: DeserializeOwned,
-  D: Serialize
+    D: Deserializer<'de>,
 {
+    let s: String = Deserialize::deserialize(deserializer)?;
+    serde_json::from_str(&s).map_err(serde::de::Error::custom)
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct QueryResultDataEntryValueBodyDeployment {
+  pub created_at: Option<DateTime<Utc>>
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct QueryResultDataEntryValueBody {
+  pub deployment: QueryResultDataEntryValueBodyDeployment
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct QueryResultDataEntryValue {
+  pub body: QueryResultDataEntryValueBody
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct QueryResultDataEntryValues {
+  pub timestamp: String,
+  #[serde(deserialize_with = "deserialize_inner")]
+  pub data: QueryResultDataEntryValue
+}
+
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct QueryResultDataEntry {
+  pub stream: QueryResultDataEntryStream,
+  pub values: Vec<QueryResultDataEntryValues>
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct QueryResultData {
+  #[serde(rename = "resultType")]
+  pub result_type: String,
+  pub result: Vec<QueryResultDataEntry>
+}
+
+#[derive(Deserialize, Debug)]
+pub struct QueryResult {
+  pub status: String,
+  pub data: QueryResultData
+}
+
+pub async fn query(data: QueryParams) -> Result<QueryResult, AppError> {
   let client = reqwest::Client::new();
   let url = env::var("LOKI_URL")?;
   let user = env::var("LOKI_USER")?;
   let password = env::var("LOKI_TOKEN")?;
 
   let response = client
-    .post(url)
-    .json(&data)
+    .get(url)
+    .query(&data)
     .basic_auth(user, Some(password))
     .send()
     .await?;
+  
+  let data: QueryResult = response.json().await?;
 
-  let data: R = response.json().await?;
-  Ok(data)
+  return Ok(data);
 }
--- src/routes/deployment_frequency.rs
+++ src/routes/deployment_frequency.rs
@@ -1,23 +1,71 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use axum::{
   http::StatusCode,
   response::Json
 };
 
-use crate::helpers::loki::query;
+use crate::helpers::loki::{query, QueryParams};
 
 #[derive(Deserialize, Debug)]
 pub struct DeploymentFrequencyRequest {
-
+  repositories: Option<Vec<String>>,
+  team: Option<String>,
+  start: DateTime<Utc>,
+  end: DateTime<Utc>
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct DeploymentFrequencyResponse {
+#[derive(Serialize, Debug, Clone)]
+pub struct DeploymentFrequencyRecord {
+  created_at: DateTime<Utc>,
+  environment: String,
+  state: String,
+  repository: String,
+  team: String
+}
 
+#[derive(Serialize, Debug, Clone)]
+pub struct DeploymentFrequencyResponse {
+  records: Vec<DeploymentFrequencyRecord>
 }
 
 pub async fn handle_request(Json(data): Json<DeploymentFrequencyRequest>) -> Result<Json<DeploymentFrequencyResponse>, StatusCode> {
-  let response : DeploymentFrequencyResponse = DeploymentFrequencyResponse{};
+  let mut response : DeploymentFrequencyResponse = DeploymentFrequencyResponse{ records: [].to_vec() };
 
-  Ok(Json(response))
+  let team_query = match data.team {
+    Some(t) => format!(r#",team_name="{}""#, t),
+    None => "".to_string()
+  };
+
+  let repo_query = match data.repositories {
+    Some(r) => format!(r#",repository_name="{}""#, r.join("|")),
+    None => "".to_string()
+  };
+
+  let params = QueryParams {
+    start: data.start.timestamp_nanos_opt().unwrap().to_string(),
+    end: data.end.timestamp_nanos_opt().unwrap().to_string(),
+    query: format!(r#"{{deployment_state=~"success|failure"{}{}}}"#, team_query, repo_query)
+  };
+
+  let query_result = query(params).await;
+
+  match query_result {
+      Ok(query_data) => {
+        response.records = query_data.data.result.iter().flat_map(|m| {
+          m.values.iter().map(|v| {
+            DeploymentFrequencyRecord {
+              created_at: v.data.body.deployment.created_at.unwrap(),
+              environment: m.stream.environment_name.clone().unwrap(),
+              repository: m.stream.repository_name.clone().unwrap(),
+              state: query_data.status.clone(),
+              team: m.stream.team_name.clone().unwrap()
+            }  
+          }).collect::<Vec<_>>()
+        }).collect::<Vec<_>>();
+
+        Ok(Json(response))
+      },
+      Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+  }
 }
